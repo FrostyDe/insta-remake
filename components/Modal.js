4db2087d7ae02e0dbddbc9dbaa1bb05c0486fe32
FrostyDe <coc.dwiki@gmail.com>
2021-11-02 15:40:15 +0700

    small fix

--- components/Modal.js
+++ components/Modal.js
@@ -163,7 +163,7 @@ function Modal() {
                     className="inline-flex justify-center w-full rounded-md border border-transparent
                     shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
                     focus:ring-2 focus:ring-offset-2 sm:text-sm disabled:bg-gray-300
-                    disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    disabled:cursor-not-allowed"
                     onClick={uploadPost}
                   >
                     {loading ? "Uploading..." : "Upload Post"}
